4f9c1ac6df2bdeae124f1f8934cabc768d363039
salasya2 <salasya2@asu.edu>
2026-06-01 10:51:49 -0700

    Time: 3 ms (32.81%), Space: 19.4 MB (87.65%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,24 +2,18 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
         res = []
-        count = defaultdict(int)
-        subset = []
 
-        for num in nums:
-            count[num] += 1
-        mask = 0
-        def dfs(subset,mask):
 
-            if len(subset) == len(nums):
-                res.append(subset.copy())
+        def dfs(idx):
+
+            if idx == len(nums):
+                res.append(nums[:])
                 return
-
-            for i in range(len(nums)):
-                if not (mask&(1<<i)):
-                    
-                    subset.append(nums[i])
-                    dfs(subset,(mask|(1<<i)))
-                    subset.pop()
-        
-        dfs(subset,mask)
+            
+            for i in range(idx, len(nums)):
+                nums[idx],nums[i] = nums[i],nums[idx]
+                dfs(idx+1)
+                nums[idx],nums[i] = nums[i],nums[idx]
+        dfs(0)
         return res
+        
