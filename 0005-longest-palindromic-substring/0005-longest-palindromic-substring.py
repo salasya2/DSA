14d65f5af5528a38e6cd3dc4bdc24452b501258e
salasya2 <salasya2@asu.edu>
2026-06-29 11:50:06 -0700

    Time: 1289 ms (28.79%), Space: 27.3 MB (5.56%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,48 +1,25 @@
 class Solution:
-
-
-    def helper(self, s):
-        n = len(s)
-        max_len = -1
-        max_substr = ""
-        for i in range(n):
-            l,h= i,i       
-            if max_len == -1:
-                max_substr = s[i]
-                max_len = 1
-            
-            while l>=0 and h<=n-1 and s[l] == s[h]:
-                l-=1
-                h+=1
-            substring = s[l+1:h]
-            if len(substring) > max_len:
-                max_substr = substring
-                max_len = len(substring)
-        for i in range(n-1):
-
-            l = i 
-            h = i + 1
-            if max_len == -1:
-                max_substr = s[i]
-                max_len = 1
-            while l>=0 and h<=n-1 and s[l] == s[h]:
-                l-=1
-                h+=1
-            substring = s[l+1:h]
-            if len(substring) > max_len:
-                max_substr = substring
-                max_len = len(substring)
-        return max_substr
-        return max_substr
-
     def longestPalindrome(self, s: str) -> str:
+        
         n = len(s)
 
-        if n == 0:
-            return ""
+        dp = [[False]* n for _ in range(n)]
 
+        resLen = -1
+        resIdx = -1
+
+        for i in range(n-1,-1,-1):
+
+            for j in range(i,n):
+
+                if s[j] == s[i] and ( j - i<= 2 or dp[i+1][j-1] ==True):
+                    dp[i][j] = True
+                    if resLen < (j- i + 1):
+                        resLen = j - i + 1
+                        resIdx = i
         
-        return self.helper(s)
+        return s[resIdx:resLen+resIdx]
+
 
 
         
