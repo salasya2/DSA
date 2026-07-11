2a1dec803b9122cd3aebceab2f87bef936b12e82
salasya2 <salasya2@asu.edu>
2026-07-11 12:57:05 -0700

    Time: 97 ms (17.53%), Space: 19.9 MB (92.66%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,37 +1,20 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
-        '''
-        have to return the flight itineracy with atmost k stops and return -1 if not possible
+        prices = [float("inf")] * n
 
-        brute force - log all the paths, filter  paths with atmost k stops and return cheapest
-        '''
+        prices[src] = 0
 
-        graph = [[] for i in range(n)]
+        for i in range(k+1):
 
-        for u,v, c in flights:
-            graph[u].append([v,c])
-        
-        dist = [[float("inf")] * (k+5) for _ in range(n)]
-        dist[src][0] = 0
-        min_heap = [[0,0,src]]
-        
-        while min_heap:
+            tmpPrices = prices.copy()
 
-            c,stops,u = heapq.heappop(min_heap)
+            for s,d,p in flights:
 
-            if u == dst:
-                return c
-            if stops == k+1 or dist[u][stops ] < c:
-                continue
-            
-            
-            for v,c_v in graph[u]:
+                if prices[s] == float("inf"):
+                    continue
                 
-                nxt_stop = stops + 1
-                if c + c_v < dist[v][nxt_stop]:
-                    dist[v][nxt_stop ] = c + c_v
-                    heapq.heappush(min_heap,[c_v+c,nxt_stop,v])
-        return -1 
-
-            
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
