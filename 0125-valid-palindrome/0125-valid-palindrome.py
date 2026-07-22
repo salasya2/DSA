d2da7daeb092115fd7f0e8d1117f36f1c5d15afd
salasya2 <salasya2@asu.edu>
2026-07-21 20:32:32 -0700

    Time: 11 ms (32.24%), Space: 19.7 MB (52.54%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        '''
+        have to deal with only alphanumeric characters -> alnum()
+        '''
+
+        i = 0 
+        j = len(s) - 1
+
+        while i < j:
+
+            while i < len(s) and not s[i].isalnum():
+                i+=1
+
+            while j >= 0 and not s[j].isalnum():
+                j -= 1
+            if  i == len(s) or  j < 0:
+                break
+            if s[i].lower() != s[j].lower():
+                
+                return False
+            i += 1
+            j -= 1 
+
+        return True 
+
+    
