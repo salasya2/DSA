9f2920530cb9ddf4413d7df94b8ab503969b0a36
salasya2 <salasya2@asu.edu>
2026-07-21 22:16:22 -0700

    Time: 4 ms (98.98%), Space: 19.5 MB (7.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,26 +8,17 @@ class Solution:
         if n == 0:
             return 0
 
-        hashMap = defaultdict(int)
-        end = 0
-        while  end < n and hashMap[s[end]] == 0:
-            hashMap[s[end]] += 1 
-            maxLen += 1
-            end += 1
+        seen = {}
         start = 0
-        print(end,maxLen)
-        for i in range(end,n):
 
-            if hashMap[s[i]] != 0:
-                
-                while hashMap[s[i]] != 0:
-                    hashMap[s[start]] -= 1
-                    start += 1
-                hashMap[s[i]] += 1
-            else:
-                
-                hashMap[s[i]]+=1
-                maxLen = max(i-start+1,maxLen)
+        for end, c in enumerate(s):
+
+            if c in seen and seen[c] >= start:
+                start = seen[c] + 1
+            
+            seen[c] = end
+
+            maxLen = max(maxLen, end - start + 1)
         
         return maxLen
 
