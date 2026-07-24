f5f34f20170b16cf7b19d55aa4b55cf65abcbd8c
salasya2 <salasya2@asu.edu>
2026-07-24 13:03:30 -0700

    Time: 95 ms (52.99%), Space: 32.9 MB (9.53%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,57 +1,55 @@
+class Node:
+
+    def __init__(self, value, Min):
+        self.value = value
+        self.min = Min
+
 class MinStack:
 
+    '''
+    
+    O(1) -> for all functions
+
+    maintain min_variable, that gets invalidated everypop
+    [21476,21476,21477,21477,-21478]
+
+    '''
+
     def __init__(self):
+        self.stack  = []
+        self.min = 2 ** 31
+       
 
-        self.stack = []
-        self.min = float("inf")
-
-    def push(self, val: int) -> None:
+    def push(self, value: int) -> None:
         
-        if not self.stack:
-
-            self.stack.append(0)
-            
-            self.min = val
-
-        else:
-
-            self.stack.append(val-self.min)
-            if val < self.min:    
-                self.min = val
+        self.min = min(value, self.min)
+        self.stack.append(Node(value,self.min))
+        
 
     def pop(self) -> None:
-        
-
         if not self.stack:
-            return None
-
-        pop = self.stack.pop()
-
-        if pop < 0:
-            self.min = self.min - pop
+            raise IndexError("Empty Stack")
+        self.stack.pop()
+        if not self.stack:
+            self.min = 2**31
+        else:
+            self.min = self.getMin()
         
 
     def top(self) -> int:
-
-        top = self.stack[-1]
-
-        if top > 0:
-
-            return top + self.min
-        
-        else:
-            return self.min
-        
-
+        if not self.stack:
+            raise IndexError("Empty Stack")
+        return self.stack[-1].value
     def getMin(self) -> int:
-
-        return self.min
+        if not self.stack:
+            raise IndexError("Empty Stack")
+        return self.stack[-1].min
         
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(val)
+# obj.push(value)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
