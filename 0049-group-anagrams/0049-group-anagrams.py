3aed5cca0ac533990cc24c356f88c312af3245f2
salasya2 <salasya2@asu.edu>
2026-07-23 17:51:25 -0700

    Time: 15 ms (41.52%), Space: 24 MB (9.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,29 +2,26 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         '''
-        list of strings -> group them together based on anagrams
-
-        Brute Force:
-        initialize the result list of lists
-        we go through each string in the list add it to the inner list
-        and then go  through second loop checking if the outer string anagram is equal to inner string anagram.
-
-        O(n^2)
-
-
-        instead of doing this construct an anagram mapping which maps word to its anagram
-        construct a mapping which maps anagram to index in res array.
+        -> group them togther
+        -> lower case letters
+        -> upper  limit = 10^4
+        -> list of lists
         
         '''
 
-        res = defaultdict(list)
+        res = {}
 
         for s in strs:
+
             count = [0] * 26
+
             for c in s:
-                count[ord(c)-ord('a')] += 1
-            res[tuple(count)].append(s)
 
-        return list(res.values())
+                count[ord(c) - ord('a')] += 1
 
+            if tuple(count) in res:
+                res[tuple(count)].append(s)
+            else:
+                res[tuple(count)] = [s]
         
+        return [r for r in res.values()]
