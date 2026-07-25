c10bd622a095bef08a0a6dd3234889d0af7de274
salasya2 <salasya2@asu.edu>
2026-07-25 13:52:38 -0700

    Time: 61 ms (77.79%), Space: 22.7 MB (43.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,19 +1,39 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        curr = root
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        while curr:
+        '''
+        - p != q
+        - a node can be parent of itself
+        - node is a lowest common ancestor of p, q if  p and q are on opposite branches of the node.
+        - else it has to be either p or q. Whichever comes first 
+        
+        '''
 
-            if p.val > curr.val and q.val > curr.val:
-                curr = curr.right
-            elif p.val < curr.val and q.val < curr.val:
-                curr = curr.left
+        temp  =root
+
+        while temp:
+
+            if p.val < temp.val < q.val or q.val < temp.val < p.val:
+                return temp
+            
+            if p.val == temp.val or q.val == temp.val:
+                return temp
+            
+            if p.val < temp.val:
+                temp = temp.left
             else:
-                return curr
+                temp = temp.right
+        
+        return root
+
+        #O(n)
+        # O(n)
+
+        
