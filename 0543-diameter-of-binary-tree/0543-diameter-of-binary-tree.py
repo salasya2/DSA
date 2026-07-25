3e901a4617ad1cde0b23c49462c9bab4ab15ab2d
salasya2 <salasya2@asu.edu>
2026-07-25 09:18:13 -0700

    Time: 3 ms (75.58%), Space: 22.3 MB (18.07%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,27 +6,33 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-
-        '''
-        diameter -> length of longest path between two nodes
-
-        1 -> 2, 3 ; 2 -> 4,5
-        '''
-        diameter = 0
-        def dfs(root):
-            nonlocal diameter
-            if not root:
-                return 0
-            left = dfs(root.left)
-            right = dfs(root.right)
-
-            diameter = max(diameter,left + right)
-
-            return 1 + max(left,right)
         
-        dfs(root)
+
+        if not root:
+            return 0
+
+        stack = [root]
+        diameter = 0
+        heights = {}
+
+
+        while stack:
+            node = stack [-1]
+
+            if (not node.left  or  node.left in heights) and (not node.right or node.right in heights):
+                stack.pop()
+
+                left_height = heights.get(node.left,0)
+                right_height = heights.get(node.right,0)
+
+                diameter= max(diameter,left_height+right_height)
+
+                heights[node] = 1 + max(left_height, right_height)
+            else:
+
+                if node.right and node.right not in heights:
+                    stack.append(node.right)
+                if node.left and node.left not in heights:
+                    stack.append(node.left)
         return diameter
 
-
-
-        
