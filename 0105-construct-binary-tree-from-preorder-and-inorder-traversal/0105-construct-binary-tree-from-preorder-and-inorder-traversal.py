3d665e33e65bc3333b6c5330a42c7580ea38aa10
salasya2 <salasya2@asu.edu>
2026-07-26 10:41:32 -0700

    Time: 167 ms (5.03%), Space: 90.9 MB (22.69%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,34 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-
-        root = preorder[0]
         idx = 0
-        mp = {}
-        for i  in range(len(inorder)):
-            mp[inorder[i]] = i
+        if len(inorder) ==0 or len(preorder) == 0:
+            return None
+        for i in range(len(preorder)):
+            if inorder[i] == preorder[0]:
+                idx = i
+                break
         
-        def dfs(l,r):
-            nonlocal idx
-            if l > r:
-                return None
-
-
-            root_val = preorder[idx]
-            idx += 1
-            mid = mp[root_val]
-            root = TreeNode(root_val)
-            root.left = dfs(l,mid-1)
-            root.right = dfs(mid+1,r)
-            return root
-        
-        return dfs(0, len(inorder) - 1)
-
-
-
-
-
-        
+        root = TreeNode(preorder[0])
+        root.left = self.buildTree(preorder[1: 1 + idx],inorder[:idx])
+        root.right= self.buildTree(preorder[ 1 + idx :], inorder[idx + 1:])
+        return root
