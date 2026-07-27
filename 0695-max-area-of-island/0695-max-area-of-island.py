955a51c59e40fbace6729dc843fb8948f283ff4a
salasya2 <salasya2@asu.edu>
2026-07-27 14:37:59 -0700

    Time: 35 ms (8.2%), Space: 19.4 MB (90.01%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,34 +1,54 @@
+class DSU:
+
+    def __init__(self,n):
+        self.parent = list(range(n+1))
+        self.size = [1] *(n+1)
+    def find(self,node):
+
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    def union(self,u,v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return False
+        
+        if self.size[pu] >= self.size[pv]:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+
+        return True
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-
-        Dir = [[-1,0],[1,0],[0,1],[0,-1]]
-
         n = len(grid)
         m = len(grid[0])
 
+        def index(i,j):
+            return i * m + j
+        
+        res = 0
 
-        def dfs(r,c,area):
+        dsu = DSU(n*m)
 
-            area += 1
-
-            grid[r][c] = 0
-
-            for dr,dc in Dir:
-
-                nr,nc = dr + r,dc + c
-
-                if nr < 0 or nc < 0 or nr >= n or nc >= m or grid[nr][nc] == 0:
-                    continue
-
-                area = max(area,dfs(nr,nc,area))
-            
-            return area
-        area = 0
         for i in range(n):
 
             for j in range(m):
 
                 if grid[i][j] == 1:
-                    area = max(area,dfs(i,j,0))
-        return area
-        
+                    res = max(res,dsu.size[index(i,j)])
+
+                    for dr,dc in [[-1,0],[1,0],[0,1],[0,-1]]:
+
+                        nr,nc = dr + i, dc + j
+
+                        if nr < 0 or nc <0 or nr >= n or nc >= m or grid[nr][nc] != 1:
+                            continue
+                        
+                        dsu.union(index(nr,nc),index(i,j))
+                    res = max(res,dsu.size[dsu.find(index(i,j))])
+        return res
