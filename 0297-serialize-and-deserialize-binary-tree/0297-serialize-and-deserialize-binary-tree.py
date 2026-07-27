fe39df16e977792d26b4365c75f0d79fe9e402d8
salasya2 <salasya2@asu.edu>
2026-07-26 21:28:47 -0700

    Time: 76 ms (97%), Space: 22.5 MB (75.69%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -5,65 +5,60 @@
 #         self.left = None
 #         self.right = None
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Codec:
-    
-    # Encodes a tree to a single string.
-    def serialize(self, root: Optional[TreeNode]) -> str:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
         
+        :type root: TreeNode
+        :rtype: str
+        """
         if not root:
             return "N"
-        
         res = []
-
         queue = deque([root])
-
+        
         while queue:
-            node = queue.popleft()
-
-            if not node:
-                res.append("N")
-            else:
-                res.append(str(node.val))
-                queue.append(node.left)
-                queue.append(node.right)
-        return ",".join(res)
             
+            for i in range(len(queue)):
 
+                curr = queue.popleft()
+                if not curr:
+                    res.append("N")
+                else:
+                    res.append(str(curr.val))
+                    queue.append(curr.left)
+                    queue.append(curr.right)      
+        return ",".join(res)
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
         
-    # Decodes your encoded data to tree.
-    def deserialize(self, data: str) -> Optional[TreeNode]:
-        vals = data.split(",")
-        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals  = data.split(",")
         if vals[0] == 'N':
             return None
-        
-        root = TreeNode(int(vals[0]))
-        queue = deque([root])
-
+        node = TreeNode(vals[0])
+        queue = deque([node])
+        root = node
         i = 1
-
         while queue:
 
             node = queue.popleft()
 
             if vals[i] != 'N':
-                node.left = TreeNode(int(vals[i]))
+                node.left = TreeNode(vals[i])
                 queue.append(node.left)
-            
             i+=1
             if vals[i] != 'N':
-                node.right = TreeNode(int(vals[i]))
+                node.right = TreeNode(vals[i])
                 queue.append(node.right)
             i+=1
         
         return root
+
+ 
         
 
 # Your Codec object will be instantiated and called as such:
