2f74d5cbd43ed12932b876e6f96f04e6de21834e
salasya2 <salasya2@asu.edu>
2026-07-29 15:04:13 -0700

    Time: 79 ms (26.21%), Space: 24 MB (89.98%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,22 +1,32 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-
-        result = []
-
-        for x , y in points:
-
-            dist = math.sqrt(x**2 + y**2)
-
-            heapq.heappush_max(result,[dist,x,y])
-
-
-            if len(result) > k:
-                heapq.heappop_max(result)
         
+        '''
+        - x,y cords & K
+        - K closest points to (0,0) -> distance from (0,0) should be the sorting factor.
+        - Euclidean dist -> sqrt((x2**2-x1**2) +(y2**2 - y1**2))
+        - answer  -> unique (no mutliple sols)
+        - any order is fine
+
+        initial appr ;- go through each point, calculate the distance and push it to the queue along with the distance
+        so it must be min_heap -> i can pop the k elements
+
+        '''
         res = []
-        for i in range(k):
-            dist, x, y = heapq.heappop_max(result)
-            res.append([x,y])
+        max_heap = []
+
+        for point in points:
+            dist = math.sqrt(point[0]**2 + point[1]**2)
+            heapq.heappush(max_heap,[-dist,point])
+
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+            #[-3,[3,0] , -2 [2,]]
+        
+        res = [point for _,point in max_heap]
         return res
 
-        
+#O(nlogk),O(k)
+
+
+
