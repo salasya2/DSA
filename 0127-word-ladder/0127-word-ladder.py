74a62e1eebfdd364664bae162fe8f739b3d58041
salasya2 <salasya2@asu.edu>
2026-07-29 10:16:35 -0700

    Time: 476 ms (17.5%), Space: 22.7 MB (17.51%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,40 +2,42 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
 
         
+        
+        n = len(wordList)
         if endWord not in wordList:
             return 0
-        graph = defaultdict(list)
+        
         wordList.append(beginWord)
+        graph = defaultdict(list)
         for word in wordList:
+
             for i in range(len(word)):
-                pattern = word[:i]+"*"+word[i+1:]
+
+                pattern = word[:i] + '*' + word[i+1:]
+
                 graph[pattern].append(word)
-
         
-        
-        q = deque([beginWord])
-        visited = set([beginWord])
-        res = 1
-        while  q:
+        res = 0
+        queue = deque([beginWord])
+        visited = set()
+        visited.add(beginWord)
 
-            for i in range(len(q)):
-                word = q.popleft()
+        while queue:
+            res +=1
+            for i in range(len(queue)):
+                
+                word = queue.popleft()
 
                 if word == endWord:
                     return res
-                
-                for j in range(len(word)):
-                    pattern = word[:j]+"*"+word[j+1:]
-                    for v in graph[pattern]:
-                        if v not in visited:
-                            visited.add(v)
-                            q.append(v)
-            res+=1
+                visited.add(word)
+                for i in range(len(word)):
+                    pattern = word[:i] + "*" + word[i+1:]
+                    for nei_word in graph[pattern]:
+                        if nei_word not in visited:
+                            queue.append(nei_word)
+            
         
         return 0
-                    
 
-
-
-
-
+        
