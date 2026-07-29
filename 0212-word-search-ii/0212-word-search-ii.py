9a005fc909ad6c74f73280aa65faf3a0e9490800
salasya2 <salasya2@asu.edu>
2026-07-29 14:51:08 -0700

    Time: 5478 ms (40.38%), Space: 21.7 MB (15.27%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,68 +1,68 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = ""
-class Solution:
+        self.isEnd = 0
+class Trie:
     def __init__(self):
         self.root = TrieNode()
-
-    def insert(self, word):
+    def insert(self,word):
         curr = self.root
-        
+
         for c in word:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
-        curr.word = word
         
-
+        curr.isEnd += 1
+    
     def search(self, word):
 
         curr = self.root
-
         for c in word:
             if c not in curr.children:
                 return False
             curr = curr.children[c]
-        return (len(curr.word) > 0)
-    
+        return curr.isEnd
+
+
+class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie  = Trie()
 
         for word in words:
-            self.insert(word)
-        
-        res = set()
-        temp = ""
-        curr = self.root
-        
-        def helper(curr, board, i, j,words):
-
-            if i >= len(board) or j >= len(board[0]) or i < 0 or j < 0 : 
-                return
-            if board[i][j] not in curr.children:
-                return
+            trie.insert(word)
+        res = []
+        def backtrack(r,c,curr):
+            nonlocal n,m
+            val = board[r][c]
+            board[r][c] = "#"
             
-            curr = curr.children[board[i][j]]
-            if len(curr.word) > 0:
-                res.add(curr.word)
-            old_char = board[i][j]
-            board[i][j] = '#'
-            helper(curr,board,i+1,j,words)
-            helper(curr,board,i-1,j,words)
-            helper(curr,board,i,j+1,words)
-            helper(curr,board,i,j-1,words)
-            board[i][j] = old_char
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+            if curr.isEnd:
+                curr.isEnd -= 1
+                res.append("".join(word))
+
+            for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+
+                nr, nc = dr + r, dc + c
+
+                if nr < 0 or nc < 0 or nr >= n or nc >=m or board[nr][nc] not in curr.children or board[nr][nc] == "#":
+                    continue
+                word.append(board[nr][nc])
+            
+                backtrack(nr,nc,curr.children[board[nr][nc]])
+                word.pop()
+            board[r][c] = val
+
+
+        root = trie.root
+        n = len(board)
+        m = len(board[0])
+        for i in range(n):
+            for j in range(m):
+                curr = root
+                
                 if board[i][j] in curr.children:
-                    # print("going in")
-                    helper(curr, board, i, j, words)
-
-        return list(res)
-            
-
-        
-
-
-        
+                    word = [board[i][j]]
+                    backtrack(i,j,curr.children[board[i][j]])
+        return res
+                       
