e4de3aec01ca83bc850e8bf7a296c4dda1b9cfb3
salasya2 <salasya2@asu.edu>
2026-07-30 16:06:53 -0700

    Time: 3 ms (24.67%), Space: 19.2 MB (70.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,21 +1,61 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-
-        n = len(s)
-        if n > 0 and s[0] == '0':
-            return 0
-        dp = [0] * (n+1)
-        dp[n] = 1
-
-        for i in range(n-1,-1,-1):
-            if i == n:
-                return 1
-            if s[i] == '0':
-                dp[i] = 0
-                continue
-            dp[i] += dp[i+1]
-            if i < n - 1:
-                if (s[i] == '1' or (s[i] == '2' and s[i+1] < '7')):
-                    dp[i] += dp[i+2]
         
-        return dp[0]
+
+        '''
+        - number in string -> try to decode the way - > matching numbers to letter
+
+        - 06 is not valid
+        
+        226 => 
+
+        0<i<2 if i == 2 then i+1 < 7
+         if decode of rest is successful -> then 2 ways
+
+        s[i:]
+
+        '''
+        res = 0 
+        dp1 = 0 
+        dp2 = 1
+        n = len(s)
+        curr = 0
+        for i in range(n):
+
+            
+            curr = 0
+            if s[i]!="0":
+                curr +=  dp2  
+            if i>0 and ( s[i-1] == "1" or (s[i-1] == "2" and s[i] < "7")):
+                curr += dp1
+                    
+            
+            dp1 = dp2
+            dp2 = curr
+                
+        return dp2
+                
+                
+
+
+                
+
+
+
+        # n = len(s)
+        # def helper(i):
+        #     nonlocal res,n
+
+        #     if i == n:
+        #         return 1
+            
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     res = helper(i+1)
+        #     if i < n - 1:
+        #         if s[i] == "1" or s[i] == "2" and s[i+1] < "7":
+        #             res += helper(i+2)
+        #     return res
+        # return helper(0)
+        
