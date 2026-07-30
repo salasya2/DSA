690738b94503b637bc94eeebfa27bc74e0f005c8
salasya2 <salasya2@asu.edu>
2026-07-29 21:33:40 -0700

    Time: 0 ms (100%), Space: 19.3 MB (51.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,13 +3,13 @@ class Solution:
         
         if n == 1:
             return 1
-        
-        l = [0] * n
 
-        l[0] = 1
-        l[1] = 2
-
+        if n == 2:
+            return 2
+        dp = [0] * n
+        dp[0] = 1 
+        dp[1] = 2
         for i in range(2,n):
-            l[i] = l[i-1] + l[i-2]
-        
-        return l[n-1]
+            dp[i] = dp[i-1]+dp[i-2]
+
+        return dp[n-1]
