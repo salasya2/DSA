d9795e4f4dc6cf287066607d524a63eb6d0f8e69
salasya2 <salasya2@asu.edu>
2026-07-30 09:08:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (31.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,26 +1,41 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        n = len(nums)
 
+        '''
+         - arranged in circle, so I can only rob one of the edges
+
+            2 , 3, 2 
+            s , N , N
+
+            2 , 3, 2
+            N,  N , s
+
+            2, 3, 2
+            N, s , s
+
+            1 , 2, 3 , 1, 4
+            N   s  n   n  S = > 6
+            N   n  s   n  s
+        '''
+        n = len(nums)
         if n == 1:
             return nums[0]
+        if n == 2:
+            return  max(nums[0],nums[1])
+        return max(self.helper(nums,1,n),self.helper(nums,0,n-1))
         
-        return max(self.helper(nums[0:n-1]),self.helper(nums[1:n]))
-    
-    def helper(self, nums):
 
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        
-        rob1 = 0
-        rob2 = 0
+    def helper(self, nums, start,end):
 
-        for num in nums:
-            temp = max(num+rob1,rob2)
-            rob1 = rob2
-            rob2 = temp
         
-        return rob2
+        one = nums[start]
+        two = max(nums[start + 1],nums[start])
+
+        for i in range(start + 2, end):
+
+            temp = two
+            two = max(one + nums[i], two)
+            one = temp
+
+        return two
