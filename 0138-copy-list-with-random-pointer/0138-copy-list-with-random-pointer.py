b8c0f5d35ac2760aa5182fc69c308aca09567b1a
salasya2 <salasya2@asu.edu>
2026-07-29 19:16:39 -0700

    Time: 50 ms (43.5%), Space: 19.9 MB (98.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -19,32 +19,18 @@ class Solution:
         mapped = {}
         if not head:
             return head
-        dummy = Node(0)
-        curr = dummy
-        temp = head
-        while temp:
-            if temp in mapped:
-                curr.next = mapped[temp]
-            else:
-                curr.next = Node(temp.val)
-                mapped[temp] = curr.next
-            random_node = temp.random
+        
+        curr = head
+
+        while curr:
+            mapped[curr] = Node(curr.val)
             curr = curr.next
-            curr_random = curr
-            
-            while random_node:
-                # print(random_node.val,temp.random.val, temp.val)
-                if random_node in mapped:
-                   
-                    curr_random.random = mapped[random_node]
-                    curr_random = curr_random.random
-                    break
-                else:
-                    curr_random.random = Node(random_node.val)
-                    mapped[random_node] = curr_random.random
-                    curr_random = curr_random.random
-                random_node = random_node.random
-            
-            temp = temp.next
+        
+        curr = head
+        while curr:
+
+            mapped[curr].next = mapped.get(curr.next)
+            mapped[curr].random = mapped.get(curr.random)
+            curr = curr.next
     
         return mapped[head]
