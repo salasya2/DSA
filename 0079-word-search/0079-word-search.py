d76070facc218d622442ffb39c028cddcd5cffff
salasya2 <salasya2@asu.edu>
2026-08-01 15:58:31 -0700

    Time: 3779 ms (52.96%), Space: 19.5 MB (57.12%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(board)
+        m = len(board[0])
+        if len(word) == 0:
+            return False
+        built = []
+        def find(i,r,c):
 
-
-        
-        def dfs(row, col, i):
+            if i > len(word):
+                return False 
             if i == len(word):
                 return True
-
-            if (row >= len(board) or col >= len(board[0]) or row < 0 or col < 0 or board[row][col] == '#' or word[i] != board[row][col]):
-                return False
             
-            
-            board[row][col] = '#'
+           
+            val = board[r][c]
+            board[r][c] = "#"
+            for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
 
-            res = (dfs(row-1,col,i+1) or dfs(row, col - 1, i+1) or dfs(row+1,col,i+1) or dfs(row,col+1,i+1))
+                nr,nc = dr+r,dc+c
 
-            board[row][col] = word[i]
-
-            return res
-        
-        for i in range(len(board)):
-
-            for j in range(len(board[0])):
-
-                if dfs(i,j,0):
+                if nr < 0 or nc <0 or nr >= n or nc >=m or board[nr][nc] != word[i] or board[nr][nc] == "#":
+                    continue
+                
+                if find(i+1,nr,nc):
                     return True
+            
+            board[r][c] = val
+            return False
         
-        return False
+        for i in range(n):
+            for j in range(m):
 
-        
+                if board[i][j] == word[0]:
+                    if find(1,i,j):
+                        return True
+        return False
