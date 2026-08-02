d0e23c2b920eac3c1f34e7cdefd18bda8168a206
salasya2 <salasya2@asu.edu>
2026-08-01 22:07:13 -0700

    Time: 9466 ms (5%), Space: 20.7 MB (8.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,36 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
-        n = len(nums)
-        goal = n - 1
-        for i in range(n-2,-1,-1):
-            if  i + nums[i] >= goal:
-                goal = i
 
-        return goal == 0
+        n = len(nums)
+        dp = [False] * n
+
+        dp[n-1] = True
+
+        for i in range(n-2,-1,-1):
+
+            for j in range(i + 1, min(n,i + nums[i] + 1)):
+
+                if dp[j]:
+                    dp[i] = True
+        print(dp)
+        return dp[0]
+        # def helper(i):
+
+        #     if i >= n-1:
+        #         return True
+        #     if nums[i] == 0:
+        #         return False
+
+        #     steps  = nums[i]
+
+        #     for j in range(1,steps+1):
+
+        #         if helper(i + j):
+        #             return True
+            
+            
+        
+        # if helper(0):
+        #     return True
+        # return False
+        #O(n^N/m) O(n^n/m)
