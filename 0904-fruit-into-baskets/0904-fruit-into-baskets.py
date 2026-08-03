fe646846498f91d35c74644681037703b59854ad
salasya2 <salasya2@asu.edu>
2026-08-03 14:11:16 -0700

    Time: 219 ms (13.06%), Space: 25.7 MB (84.77%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,43 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        
+
+        n = len(fruits)
+
+        '''
+        
+        1 , 2, 1, 2, 3 , 3
+
+        3 -> 2
+        2 -> 2
+        1 -> 2
+        
+        
+        '''
+
+        i = 0
+        j = 0
+        res = 0
+        seen = {}
+        # 1, 2 , 3, 2, 4
+        while i < n and j < n:
+
+            if len(seen) == 2 and fruits[j] not in seen:
+                print(seen)
+                print(res)
+                idx = j
+                for fr,fruit_id in seen.items():
+                    idx = min(fruit_id,idx)
+                start = fruits[idx]
+                i = idx + 1
+                del seen[start]
+
+            seen[fruits[j]] = j
+            res = max(res,j - i + 1)
+            j += 1
+            
+        # res = max(res,j - i)
+        return res
+
+
+
