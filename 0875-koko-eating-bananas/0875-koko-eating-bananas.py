77825f98ee62ed1acfe8d392bbf1c93780b4bf7c
salasya2 <salasya2@asu.edu>
2026-08-03 15:06:39 -0700

    Time: 175 ms (40.54%), Space: 20.6 MB (42.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,40 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-
-        '''
-        
-        h - > no of hours to complete the  bananas
-        piles[i] -> no of bananas per pile
-
-        if piles[i] > k then it should take more than 1 hr
-        if piles[i] <= k -> 1 hour
-
-        max of k will be < max of piles
-        min of k will be let's say 0
-
-        '''
-        
-        r = max(piles)
+        piles.sort() 
         l = 1
-
+        r = piles[-1]
         n = len(piles)
-        
+        res = 0
         while l <= r:
-
-            mid = l + (r - l)//2
-
-            time_taken = 0
+            m = l + (r - l)//2
+            count = 0
             for p in piles:
 
-                time_taken += (p + mid - 1)//mid
-                
-            if time_taken <= h:
-                r = mid - 1
+                count += math.ceil(p/m)
+            # print(count)
+            if count <= h:
+                res = m
+                r = m - 1
             else:
-                l = mid + 1
-            
-        return l
-
-
-
+                l = m + 1
         
+        return res
