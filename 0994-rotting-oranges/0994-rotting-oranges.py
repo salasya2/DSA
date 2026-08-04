85fd77cfe21782d814eb5ea39e4bf05a6413a4f8
salasya2 <salasya2@asu.edu>
2026-08-04 15:30:41 -0700

    Time: 7 ms (16.18%), Space: 19.7 MB (12.65%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,38 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-
-
-        res = 0
 
         queue = deque()
+        n = len(grid)
+        m = len(grid[0])
         fresh = 0
+        res = 0
+
         for i in range(n):
             for j in range(m):
+
                 if grid[i][j] == 2:
-                    grid[i][j] = 10000000
                     queue.append([i,j,0])
                 if grid[i][j] == 1:
                     fresh+=1
-        
-
+        if fresh == 0:
+            return 0
         while queue:
+
+            r,c,time = queue.popleft()
+            
+            print(r,c,time)
             if fresh == 0:
                 return res
-            for i in range(len(queue)):
-                r,c,dist = queue.popleft()
-                
-                for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-                    nr , nc = dr + r, dc + c
+            
+            for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+                nr,nc = dr + r, dc + c
 
-                    if nr < 0 or  nc < 0 or nr >= n or nc >= m or grid[nr][nc] != 1:
-                        continue
-                    queue.append([nr,nc,dist+1])
-                    grid[nr][nc] = 2
-                    fresh -=1
-                
-                    res= max(res,dist+1)
-        if fresh == 0:
-            return res
+                if nr < 0 or nc < 0 or nr >= n or nc >= m or grid[nr][nc] != 1:
+                    continue
+                fresh -= 1
+                grid[nr][nc] = 2
+                queue.append([nr,nc,time+1])
+                res = max(time + 1,res)
+        
         return -1
